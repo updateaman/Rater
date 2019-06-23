400d73205ab3baa76887dba3882f7755be06cd0c
Aman Baloch <abaloch05@hotmail.com>
2019-06-23 17:45:21 +0100

    initial commit

---
+++ RaterWeb/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AddItemComponent } from './add-item/add-item.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AddItemComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
